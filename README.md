2bfeac823bc5a37442e07454cef66b9828a9f657
Phillip J. Murphy <phillipjmurphy@betaxanalytics.com>
2018-05-09 08:49:39 -0400

    Shift over

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # infiniteanalytics
+
 Data Science Tools
+
+## Book
+
+[ThinkStats2](http://greenteapress.com/thinkstats2/thinkstats2.pdf)
--- iaTools/explore.py
+++ iaTools/explore.py
@@ -15,3 +15,24 @@ def data_frame_date_hist(col):
     ax.xaxis.set_major_locator(month_loc)
     ax.xaxis.set_major_formatter(date_format)
     plt.xticks(rotation=45)
+
+def test_code():
+    pass
+
+def get_percent_null(col):
+    # the df isn't global.
+    if df[col].hasnans:
+        return 100 * df[col].isnull().value_counts().loc[True] / df[col].shape[0]
+    else:
+        return 0.00
+
+def get_has_nans(col):
+    return df[col].hasnans
+def get_is_unique(col):
+    return df[col].is_unique
+
+def another_test():
+    df_meta = pd.DataFrame(df.columns)
+    df_meta['pct_null'] = df_meta['COL_NAME'].apply(get_percent_null)
+    df_meta['hasnans'] =  df_meta['COL_NAME'].apply(get_has_nans)
+    df_meta['is_unique'] =  df_meta['COL_NAME'].apply(get_is_unique)
